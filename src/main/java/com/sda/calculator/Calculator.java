de59556c76a0d4d8cffb43cd23debcae11bfddd7
Sebastian Wojnicz <sebulw@gmail.com>
2018-08-14 15:59:37 +0200

    Add method can get more than 2 values now

--- src/main/java/com/sda/calculator/Calculator.java
+++ src/main/java/com/sda/calculator/Calculator.java
@@ -6,24 +6,16 @@ import java.util.List;
 
 public class Calculator {
 
-
-    public int add(String a, String b) {
-        int numberA=Integer.parseInt(a);
-        int numberB=Integer.parseInt(a);
-        return numberA+numberB;
-
-    }
-
-    public int add(String ab){
-        if(ab.equals("")){
+    public int add(String ab) {
+        int sum = 0;
+        if (ab.equals("")) {
             return 0;
         }
         List<String> list = Arrays.asList(ab.split(","));
-        int a = Integer.parseInt(list.get(0));
-        int b = Integer.parseInt(list.get(1));
-        return a+b;
-
-
+        for (String number : list) {
+            sum += Integer.parseInt(number);
+        }
+        return sum;
     }
 
     public int subtract(String ab) {
